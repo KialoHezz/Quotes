efcb0307f73068699ed98d13db1753af825e7152
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-04-08 23:53:50 +0300

    Generate form,DisplayQoutes component, imported google fonts,style the components with css and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { DisplayQouteComponent } from './display-qoute/display-qoute.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    DisplayQouteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-qoute/display-qoute.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-qoute',
+  templateUrl: './display-qoute.component.html',
+  styleUrls: ['./display-qoute.component.css']
+})
+export class DisplayQouteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
